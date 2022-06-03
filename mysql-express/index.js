d716220da6e25d25d3a083f864af26a4d8528bab
mroxlong <106468433+mroxlong@users.noreply.github.com>
2022-06-03 19:25:10 +0200

    Update index.js

--- mysql-express/index.js
+++ mysql-express/index.js
@@ -1,6 +1,34 @@
 require('dotenv').config()
 const express = require('express');
+const mysql = require('mysql')
 const app = express();
+
+app.get('')
+
+const db = mysql.createConnection({
+    host:process.env.DBHOST,
+    database:process.env.DB,
+    user:process.env.DBUSER,
+    password:process.env.DBPASS
+
+})
+
+db.connect((err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log('Mysql initialized')
+})
+
+app.get("/mail/",(req, res)=>{
+    let sql = "CREATE DATABASE ?"
+    db.query(sql, req.params.db,(err, result)=>{
+            if(err){
+                throw err
+            }
+            console.log(result);
+    })
+})
 
 
 
