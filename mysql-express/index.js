54d81930057ad01d19ea31aa0f51a9cef8ff32b6
mroxlong <106468433+mroxlong@users.noreply.github.com>
2022-06-03 23:21:29 +0200

    fixes to rooms

--- mysql-express/index.js
+++ mysql-express/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql')
 const app = express();
 const bodyParser = require('body-parser');
-const json = require('body-parser/lib/types/json');
+
 
 app.use(bodyParser.json())
 
@@ -12,8 +12,6 @@ const db = mysql.createConnection({
     database:process.env.DB,
     user:process.env.DBUSER,
     password:process.env.DBPASS,
-    
-
 })
 
 /**
@@ -45,8 +43,29 @@ app.post("/room/:id",(req, res)=>{
             if(err){
                 throw err
             }
-            console.log(result);
+            
             res.sendStatus(200)
+    })
+})
+
+app.get('/rooms',(req, res)=>{
+    let sql = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '"+process.env.DB+"'"
+    console.log(sql)
+   
+    db.query(sql,(err, result)=>{
+            const arr = []
+            if(err){
+                throw err
+            }
+            result.map((index)=>{
+               arr.push(index.TABLE_NAME)
+            })
+
+            console.log(arr);
+            db.end()
+            res.json({"rooms": arr})
+            res.sendStatus(200)
+            
     })
 })
 
@@ -60,6 +79,7 @@ app.post("/message/:room",(req, res)=>{
        room: sqlSanitize(req.params.room),
        message: sqlSanitize(req.body.message)
     }
+
     let sql = "INSERT INTO `"+params.room+"` (`id`, `uid`, `value`) VALUES (NULL, '2', '"+params.message+"')"
     console.log(sql)
    
@@ -68,6 +88,32 @@ app.post("/message/:room",(req, res)=>{
                 throw err
             }
             console.log(result);
+            db.end()
+            res.sendStatus(200)
+    })
+})
+
+
+
+
+app.post("/message/:room",(req, res)=>{
+    console.log("User creation init")
+    let msg = req.body.message
+    let params ={
+      
+       room: sqlSanitize(req.params.room),
+       message: sqlSanitize(req.body.message)
+    }
+
+    let sql = "INSERT INTO `"+params.room+"` (`id`, `uid`, `value`) VALUES (NULL, '2', '"+params.message+"')"
+    console.log(sql)
+   
+    db.query(sql,(err, result)=>{
+            if(err){
+                throw err
+            }
+            console.log(result);
+            db.end()
             res.sendStatus(200)
     })
 })
